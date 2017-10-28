00e9830da92468911e328f7250cc21ba1dc0f1d1
Christian Pehle <christian.pehle@gmail.com>
2017-10-28 11:47:27 +0200

    Cleanup + remove coverage

--- examples/embedding/embedding.cpp
+++ examples/embedding/embedding.cpp
@@ -1,17 +1,25 @@
 #include "hobbes/hobbes.H"
 #include "linenoise/linenoise.h"
 #include "llvm/Support/CommandLine.h"
+#include "llvm/LineEditor/LineEditor.h"
 
 #include <iostream>
 #include <stdexcept>
 
 int applyDiscreteWeighting(int x, int y) { return x * y; }
 
+
 int main(int argc, char *argv[]) {
+  llvm::cl::SetVersionPrinter([](){
+    std::cout << "hobbes version 0.1" << std::endl;
+  });
   llvm::cl::ParseCommandLineOptions(argc, argv, "embedding");
+  
   // hobbes context  
   hobbes::cc c;
   c.bind("applyDiscreteWeighting", &applyDiscreteWeighting);
+  c.bind("dumpModule", memberfn(&hobbes::cc::dumpModule));
+  c.bind("compiler", &c);
 
   char * read_line;
   while ((read_line = linenoise("> ")) != NULL) {
